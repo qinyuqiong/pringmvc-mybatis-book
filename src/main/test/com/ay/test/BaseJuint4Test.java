ea824c4a7946855f836a51ba6aad01355043df4b
qinyuqiong <2824019551@qq.com>
2019-09-24 11:02:41 +0800

    加入了分页功能，能查到后端信息，但分页功能不能使用

--- src/main/test/com/ay/test/BaseJuint4Test.java
+++ src/main/test/com/ay/test/BaseJuint4Test.java
@@ -3,8 +3,10 @@ package com.ay.test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath:applicationContext.xml","classpath:spring-mvc.xml"})
 public class BaseJuint4Test {
 }
